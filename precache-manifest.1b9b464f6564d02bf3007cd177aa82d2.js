e5c4d005b631bd0373a301f788192fd8cf20fafc
Mitch Constantinescu <Mitch.Constantine@gmail.com>
2020-05-10 23:04:58 -0400

    Updates

---
+++ precache-manifest.1b9b464f6564d02bf3007cd177aa82d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c7d2360c81b154ba2a3572aff6cf22c",
+    "url": "/dog-list/index.html"
+  },
+  {
+    "revision": "075551de2445f0eec83a",
+    "url": "/dog-list/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "490fabebc8b664ea05e2",
+    "url": "/dog-list/static/js/2.34beca73.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/dog-list/static/js/2.34beca73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "075551de2445f0eec83a",
+    "url": "/dog-list/static/js/main.b789385d.chunk.js"
+  },
+  {
+    "revision": "0115d2a618b0fcff9e23",
+    "url": "/dog-list/static/js/runtime-main.88946a23.js"
+  }
+]);
